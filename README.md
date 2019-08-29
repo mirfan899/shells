76fe81a55eca525d7734cdb24d140e18c81e8a80
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-08-29 11:02:29 +0500

    minor changes.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# shells
+### shells
 shell script to copy necessary files for tts for merlin and Ossian
+
+### Merlin
+`merlin_copy.sh` convert and copies the audio and text data to proper location 
---
+++ merlin_copy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#convert .m4a to .wav files
+for entry in *.m4a
+do
+  filename=$(basename -- "$entry")
+  filename="${filename%.*}"
+  ffmpeg -i "${entry}" "${filename}.wav";
+done
+
+# convert .wav to 16khz 16 bit wav
+mkdir temp
+temp_dir=./temp
+for i in *.wav; do
+  ffmpeg -i ${i} -acodec pcm_s16le -ac 1 -ar 16000 ${temp_dir}/${i};
+done
+
+unzip
+# copy to merlin directory
+rm merling/egs/cantonese_voice/s1/database/wav/*.wav
+cp temp/*.wav merling/egs/cantonese_voice/s1/database/wav
+rm -rf temp

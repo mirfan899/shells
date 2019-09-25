9844be2fcce89da83bc87b7764aa37045bb60e9a
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-09-25 18:08:44 +0500

    compile python from source

---
+++ python.sh
@@ -0,0 +1,7 @@
+wget wget https://www.python.org/ftp/python/3.6.5/Python-3.6.8.tgz
+tar xvf Python-3.6.8.tgz
+cd Python-3.6.8
+
+./configure --enable-optimizations --enable-shared
+make -j 2
+make altinstall

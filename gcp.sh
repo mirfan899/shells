308e52a534d8b50043b609b0dfda995fb8f7f904
Muhammad Irfan <virtuoso.irfan@gmail.com>
2019-10-21 12:58:27 +0500

    disk resize command added.

--- gcp.sh
+++ gcp.sh
@@ -12,3 +12,9 @@ gcloud compute scp --recurse files instance-x:/path
 
 ### from another vm to this vm
 gcloud compute scp --recurse instance-x:/path files
+
+### Resize VM disk size on GCP
+gcloud compute disks resize disk_name --size 64GB --zone xxxx-xxxxxxxx-x
+# increase sda size
+growpart /dev/sda 1
+resize2fs /dev/sda1
